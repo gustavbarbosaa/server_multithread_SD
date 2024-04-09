db84b9b6bd4c94cad35989be0a262c9f869b0f5d
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-04-09 09:22:33 -0300

    feat: cria funcao que o cliente escolhe a opcao que deseja executar e envia para o servidor

--- src/Client.java
+++ src/Client.java
@@ -8,5 +8,22 @@ public class Client {
     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
     Scanner scanner = new Scanner(System.in);
+
+    String msg;
+
+    while ((msg = in.readLine()) != null) {
+      if (msg.equals("FIM")) {
+        break;
+      }
+      System.out.println("Servidor -> " + msg);
+    }
+
+    sendResponseServer(out, scanner);
+  }
+
+  public static void sendResponseServer(PrintWriter out, Scanner scanner) {
+    System.out.println("Resposta para o servidor: ");
+    String response = scanner.nextLine();
+    out.println(response);
   }
 }
