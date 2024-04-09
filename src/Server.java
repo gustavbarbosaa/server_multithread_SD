658ec5645cee7c2ee14f9e459b62662946ad1e98
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-04-09 09:05:14 -0300

    feat: cria o servidor multithread

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,29 @@
-public class Server {
+import java.io.*;
+import java.net.*;
+
+public class Server implements Runnable {
+  public Socket client;
+
+  public Server(Socket cli) {
+    this.client = cli;
+  }
+
   public static void main(String[] args) {
-    
+    try (ServerSocket serverSocket = new ServerSocket(8080)) {
+      while (true) {
+        Socket socket = serverSocket.accept();
+        Server server = new Server(socket);
+        Thread thread = new Thread(server);
+        thread.start();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Cliente conectado: " + client.getInetAddress().getHostAddress());
+    throw new UnsupportedOperationException("Unimplemented method 'run'");
   }
 }
