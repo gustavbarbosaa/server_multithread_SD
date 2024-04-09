356f9fc24ae64102a4cb317d1e0667b1513b5f68
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-04-09 09:09:00 -0300

    feat: conecta com o servidor e cria as variaveis de comunicacao entre o servidor e o cliente

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,12 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws UnknownHostException, IOException {
+    Socket socket = new Socket("localhost", 8080);
+    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    Scanner scanner = new Scanner(System.in);
   }
 }
