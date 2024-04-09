f9ff56ce437a9f662aee5cb73257dc5772aa1933
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-04-09 10:04:43 -0300

    feat: cria funcionalidade para listar todas as imagens no DAO

--- src/DAO/ImageDAO.java
+++ src/DAO/ImageDAO.java
@@ -3,6 +3,7 @@ package DAO;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -59,5 +60,21 @@ public class ImageDAO {
       e.printStackTrace();
       return false;
     }
+  }
+
+  public ArrayList<String> getAllImages() {
+    ArrayList<String> imageNames = new ArrayList<>();
+    String sql = "SELECT name_image FROM images";
+
+    try(PreparedStatement preparedStatement = ConnectDB.getConnection().prepareStatement(sql)) {
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()) {
+          String name = resultSet.getString("name_image");
+          imageNames.add(name);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return imageNames;
   }
 }
