562c9f6d0d3480c0da9d6997df942a80f692d762
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-04-10 20:47:41 -0300

    :wastebasket: remove: remove arquivos do docker

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:21
-COPY ./out/production/app_multithread/Server.class /tmp
-COPY ./out/production/app_multithread/Client.class /tmp
-WORKDIR /tmp
-ENTRYPOINT ["java", "Server", "Client"]
--- src/Connection/ConnectDB.java
+++ src/Connection/ConnectDB.java
@@ -1,15 +1,26 @@
 package Connection;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class ConnectDB {
-  private static final String url = "jdbc:mysql://localhost:3306/images";
-  private static final String user = "root";
-  private static final String password = "Guga1957!";
-
   private static Connection connect;
 
   public static Connection getConnection() throws SQLException {
+    Properties properties = new Properties();
+    try (FileInputStream fis = new FileInputStream("config.properties")) {
+      properties.load(fis);
+      fis.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    String url = properties.getProperty("database.url");
+    String user = properties.getProperty("database.user");
+    String password = properties.getProperty("database.password");
+
     if (connect == null) {
       connect = DriverManager.getConnection(url, user, password);
       return connect;
